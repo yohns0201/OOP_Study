e4c41ab26d47ce96d5b61045ba249ed175bcfe82
yohns0201 <yongsooshin81@gmail.com>
2024-09-11 11:20:41 +0900

    1

---
+++ src/example1/helloWorld.java
@@ -0,0 +1,12 @@
+package example1;
+
+public class helloWorld {
+    public static void main(String[] args) {
+        String name = "SHIN Yongsoo";
+        String stdNum = "201902017";
+
+        System.out.println("Hello World!");
+        System.out.println(name);
+        System.out.println(stdNum);
+    }
+}
